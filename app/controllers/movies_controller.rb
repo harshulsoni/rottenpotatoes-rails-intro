13fc9dd52dfd0977ec84dd529e1cf6b39a01753a
EC2 Default User <ec2-user@ip-172-31-43-192.us-east-2.compute.internal>
2019-09-28 19:50:32 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,18 +12,44 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.get_rating_list
-    @ratings = @all_ratings
+    redirect = false
+    #session.clear
     if params.key?("ratings")
       @ratings = params["ratings"].keys
+      session["ratings"] = params["ratings"]
+    elsif session.key?("ratings")
+      @ratings = session["ratings"].keys
+      params["ratings"] = session["ratings"]
+      redirect = true
+    else
+      @ratings = @all_ratings
     end
     @movies = Movie.with_ratings(@ratings)
-    @movies= case params[:sortOrder]
-      when "title", "release_date"
-        instance_variable_set("@klass_#{params["sortOrder"]}", "hilite")
-        @movies.order(params["sortOrder"])
-    else
-      @movies
+    
+    sortOrder = ''
+    if params.key?(:sortOrder)
+      sortOrder = params[:sortOrder]
+      session[:sortOrder] = params[:sortOrder]
+    elsif session.key?(:sortOrder)
+      sortOrder = session[:sortOrder]
+      params[:sortOrder] = session[:sortOrder]
+      redirect = true
     end
+    
+    puts params
+    
+    if redirect
+      #redirect_to root_path(redirect_params)
+      redirect_to movies_path(params), :method => :get
+    end
+    
+    @movies= case sortOrder
+      when "title", "release_date"
+        instance_variable_set("@klass_#{sortOrder}", "hilite")
+        @movies.order(sortOrder)
+      else
+        @movies
+      end
     
   end
   
